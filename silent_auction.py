9cf2db228efb3ca0fd22850d74bbfc1f794242fc
Isaac Awotwe <isaacawotwe@gmail.com>
2025-03-09 18:24:21 -0600

    Define own function for determining highest bidder

--- silent_auction.py
+++ silent_auction.py
@@ -2,23 +2,36 @@
 from art import logo
 print(logo)
 
+
+# define a function that prints out the highest bidder
+def find_highest_bidder(bidding_dictionary):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
 # create an empty dictionary to store the bids
 auction = {}
 
+# define a parameter whose value drive the coninuation or otherwise of the loop
 keep_bidding = True
 
+#while loop
 while keep_bidding:
     # Ask the user for input
     name = input("What is your name?: ")
     bid = float(input("What is your bid?: $"))
     auction[name] = bid
-    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n")
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n").lower()
     if other_bidders == 'yes':
         print("\n" * 100)
-    else:
+    elif other_bidders == "no":
         keep_bidding = False
 
-max_key = max(auction, key = auction.get)
-
 print(f"These are the bids that came in: {auction}")
-print(f"The winner is {max_key} with a bid of ${auction[max_key]}")
+find_highest_bidder(auction)
+
